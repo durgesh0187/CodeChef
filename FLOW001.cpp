0b452aee7c4a8ad8004411d382db243d1c148837
Durgesh Kumar <68027282+durgesh0187@users.noreply.github.com>
2020-08-07 02:59:00 +0530

    commit

---
+++ FLOW001.cpp
@@ -0,0 +1,18 @@
+// bits/stdc++.h works in linux. 
+// It loads most of the libraries of C++ required.
+#include <bits/stdc++.h> 
+
+using namespace std;
+
+int main() {
+	// Read the number of test cases.
+	int T;
+	cin>>T;
+	for(int i=0;i<T;i++)
+	{
+	    int A,B;
+	    cin>>A>>B;
+	    cout<<A+B<<endl;
+	}
+	return 0;
+}
---
+++ FLOW006.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <sstream>
+using namespace std;
+
+int main() {
+    
+	int T;
+	cin>>T;
+	while(T--)
+	{
+	    int n;
+	    cin>>n;
+	    ostringstream str1;
+	    str1<<n;
+	    string s = str1.str();
+	    
+	    long long int sum=0;
+	    int len = s.length();
+	    for(int i=0;i<len;i++)
+	    {
+	        char x=s[i];
+	        int p = (int)x;
+	        p=p-48;
+	        sum=sum+p;
+	    }
+	    cout<<sum<<endl;
+	    
+	}
+	return 0;
+}
